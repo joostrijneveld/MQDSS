b466d9af09ed7f6610cfd5e9afae56458a5f817e
Ming-Shing Chen <mschen@crypto.tw>
2017-08-27 01:59:44 +0800

    Add ref version for mq n64 m64.

--- 5pass_sign/ref/c/mq.c
+++ 5pass_sign/ref/c/mq.c
@@ -3,58 +3,256 @@
 #include "mq.h"
 #include "params.h"
 
-// This is very slow and should be overridden for your particular platform
-void MQ(gf31 *fx, const gf31 *x, const signed char *F)
+
+#if 0
+
+inline static
+void generate_quadratic_terms_n64( gf31 * xij , const gf31 * x )
 {
     int i, j, k;
-    gf31 xixj;
-    int Fi = 0;
-
-    for (i = 0; i < M; i++) {
-        fx[i] = 0;
-    }
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j <= i; j++) {
-            xixj = (x[i] * x[j]) % 31;
-            for (k = 0; k < M; k++) {
-                fx[k] += (xixj * (F[Fi] + 31));
-                Fi++;
-            }
-        }
-        for (k = 0; k < M; k++) {
-            fx[k] %= 31;
-        }
-    }
-
-    for (i = 0; i < N; i++) {
-        for (k = 0; k < M; k++) {
-            fx[k] += x[i] * (F[Fi++] + 31);
-            fx[k] %= 31;
+    k=0;
+    for(i=0;i<N;i++) {
+        for(j=0;j<=i;j++) {
+            xij[k] = (x[i]*x[j])%31;
+            k++;
         }
     }
 }
 
-void G(gf31 *fx, const gf31 *x, const gf31 *y, const signed char *F)
+inline static
+void generate_xiyj_p_xjyi_terms_n64( gf31 * xij , const gf31 * x , const gf31 * y )
 {
     int i, j, k;
-    gf31 xiyj_plus_xjyi;
-    int Fi = 0;
+    k=0;
+    for(i=0;i<N;i++) {
+        for(j=0;j<=i;j++) {
+            xij[k] = (x[i]*y[j]+x[j]*y[i])%31;
+            k++;
+        }
+    }
+}
 
-    for (i = 0; i < M; i++) {
-        fx[i] = 0;
+
+
+#else
+
+
+inline static
+void pack_store( gf31 * xij , const gf31 * x1 , const gf31 * x2 )
+{
+    int j;
+    for(j=0;j<8;j++) { xij[   j] = x1[j]; }
+    for(j=0;j<8;j++) { xij[8 +j] = x2[j]; }
+    for(j=0;j<8;j++) { xij[16+j] = x1[8+j]; }
+    for(j=0;j<8;j++) { xij[24+j] = x2[8+j]; }
+}
+
+inline static
+void gen_quad_rec_n16( gf31 * xij , const gf31 * xi , const gf31 * xj )
+{
+    int i, j, k;
+    gf31 x1[16];
+    gf31 x2[16];
+    k=0;
+    for(i=0;i<16;i+=2) {
+        for(j=0;j<16;j++) x1[j] = (xi[i  ]*xj[j])%31;
+        for(j=0;j<16;j++) x2[j] = (xi[i+1]*xj[j])%31;
+
+        pack_store( xij + k , x1 , x2 );
+        k += 32;
+    }
+}
+
+static inline
+void ror_16x2( gf31 * r , const gf31 * x , unsigned n ) {
+    unsigned i;
+    for(i=0;i<16;i++) r[ (i+16-n)&15 ] = x[i];
+    for(i=0;i<16;i++) r[ ((i+16-n)&15) +16] = x[i +16];
+}
+
+inline static
+void generate_quadratic_tri_n32( gf31 * xij , const gf31 * x )
+{
+    int i, j, k;
+    gf31 x1[16];
+    gf31 x2[16];
+    gf31 x_rot[32];
+
+    k=0;
+    gen_quad_rec_n16( xij , x , x+16 );
+    k += 16*16;
+
+    for(i=0;i<8;i++) {
+        ror_16x2( x_rot , x , i );
+        for(j=0;j<16;j++) x1[j] = (   x[j]*x_rot[j]   )%31;
+        for(j=0;j<16;j++) x2[j] = (x[j+16]*x_rot[j+16])%31;
+        pack_store( xij + k , x1 , x2 );
+        k += 32;
     }
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j <= i; j++) {
-            xiyj_plus_xjyi = (x[i] * y[j] + x[j] * y[i]) % 31;
-            for (k = 0; k < M; k++) {
-                fx[k] += xiyj_plus_xjyi * (F[Fi] + 31);
-                Fi++;
-            }
+    ror_16x2( x_rot , x , 8 );
+    for(j=0;j<16;j++) x1[j] = (   x[j]*x_rot[j]   )%31;
+    for(j=0;j<16;j++) x2[j] = (x[j+16]*x_rot[j+16])%31;
+    for(j=0;j<8;j++) xij[ k + j ] = x1[j];
+    k += 8;
+    for(j=0;j<8;j++) xij[ k + j ] = x2[j];
+}
+
+inline static
+void generate_quadratic_rec_n32( gf31 * xij , const gf31 * xi , const gf31 * xj )
+{
+    int i, j, k;
+    k=0;
+    for(i=0;i<32;i++) {
+        for(j=0;j<32;j++) {
+            xij[k] = (xi[i]*xj[j])%31;
+            k++;
         }
-        for (k = 0; k < M; k++) {
-            fx[k] %= 31;
+    }
+}
+
+inline static
+void generate_quadratic_terms_n64( gf31 * xij , const gf31 * x )
+{
+    int i;
+    gf31 xi[64];
+    for( i=0;i<64;i++) xi[i] = ((x[i]%31)+31)%31;
+    generate_quadratic_tri_n32( xij , xi );
+    generate_quadratic_tri_n32( xij+(32*33/2) , xi+32 );
+    generate_quadratic_rec_n32( xij+(32*33) , xi , xi+32 );
+}
+
+//////////////////////////////
+
+inline static
+void gen_xiyj_p_xjyi_rec_n16( gf31 * xij , const gf31 * xi , const gf31 * xj , const gf31 * yi , const gf31 * yj )
+{
+    int i, j, k;
+    gf31 x1[16];
+    gf31 x2[16];
+    k=0;
+    for(i=0;i<16;i+=2) {
+        for(j=0;j<16;j++) x1[j] = (xi[i  ]*yj[j]+yi[i  ]*xj[j])%31;
+        for(j=0;j<16;j++) x2[j] = (xi[i+1]*yj[j]+yi[i+1]*xj[j])%31;
+
+        pack_store( xij + k , x1 , x2 );
+        k += 32;
+    }
+}
+
+inline static
+void generate_xiyj_p_xjyi_tri_n32( gf31 * xij , const gf31 * x , const gf31 * y )
+{
+    int i, j, k;
+    gf31 x1[16];
+    gf31 x2[16];
+    gf31 x_rot[32];
+    gf31 y_rot[32];
+
+    k=0;
+    gen_xiyj_p_xjyi_rec_n16( xij , x , x+16 , y , y+16 );
+    k += 16*16;
+
+    for(i=0;i<8;i++) {
+        ror_16x2( x_rot , x , i );
+        ror_16x2( y_rot , y , i );
+        for(j=0;j<16;j++) x1[j] = (   x[j]*y_rot[j]   +   y[j]*x_rot[j]   )%31;
+        for(j=0;j<16;j++) x2[j] = (x[j+16]*y_rot[j+16]+y[j+16]*x_rot[j+16])%31;
+        pack_store( xij + k , x1 , x2 );
+        k += 32;
+    }
+
+    ror_16x2( x_rot , x , 8 );
+    ror_16x2( y_rot , y , 8 );
+    for(j=0;j<16;j++) x1[j] = (   x[j]*y_rot[j]   +   y[j]*x_rot[j]   )%31;
+    for(j=0;j<16;j++) x2[j] = (x[j+16]*y_rot[j+16]+y[j+16]*x_rot[j+16])%31;
+    for(j=0;j<8;j++) xij[ k + j ] = x1[j];
+    k += 8;
+    for(j=0;j<8;j++) xij[ k + j ] = x2[j];
+}
+
+inline static
+void generate_xiyj_p_xjyi_rec_n32( gf31 * xij , const gf31 * xi , const gf31 * xj , const gf31 * yi , const gf31 * yj )
+{
+    int i, j, k;
+    k=0;
+    for(i=0;i<32;i++) {
+        for(j=0;j<32;j++) {
+            xij[k] = (xi[i]*yj[j]+yi[i]*xj[j])%31;
+            k++;
         }
+    }
+}
+
+inline static
+void generate_xiyj_p_xjyi_terms_n64( gf31 * xij , const gf31 * x , const gf31 * y )
+{
+    int i;
+    gf31 xi[64];
+    for( i=0;i<64;i++) xi[i] = ((x[i]%31)+31)%31;
+    gf31 yi[64];
+    for( i=0;i<64;i++) yi[i] = ((y[i]%31)+31)%31;
+    generate_xiyj_p_xjyi_tri_n32( xij , xi , yi );
+    generate_xiyj_p_xjyi_tri_n32( xij+(32*33/2) , xi+32 , yi+32 );
+    generate_xiyj_p_xjyi_rec_n32( xij+(32*33) , xi , xi+32  , yi , yi+32 );
+}
+
+
+#endif
+
+
+// This is very slow and should be overridden for your particular platform
+void MQ(gf31 *fx, const gf31 *x, const signed char *F)
+{
+    int i, j;
+    gf31 _xij[N*(N+1)/2];
+    int Fi = 0;
+
+    generate_quadratic_terms_n64( _xij , x );
+
+    int r[M] = {0};
+
+    for(i=0;i<N;i+=2) {
+        for(j=0;j<M;j++) {
+            r[j] += ((int)x[i])*((int)F[Fi]) + ((int)x[i+1])*((int)F[Fi+1]);
+            Fi += 2;
+        }
+    }
+
+    for(i=0;i<(N*(N+1))/2;i+=2) {
+        for(j=0;j<M;j++) {
+            r[j] += ((int)_xij[i])*((int)F[Fi]) + ((int)_xij[i+1])*((int)F[Fi+1]);
+            Fi += 2;
+        }
+    }
+
+    for (i = 0; i < M; i++) {
+        fx[i] = ((r[i]%31)+31)%31;
+    }
+}
+
+
+void G(gf31 *fx, const gf31 *x, const gf31 *y, const signed char *F)
+{
+
+    int i, j;
+    gf31 _xij[N*(N+1)/2];
+    int Fi = 0;
+
+    generate_xiyj_p_xjyi_terms_n64( _xij , x , y );
+
+    int r[M] = {0};
+
+    Fi += N*M;
+
+    for(i=0;i<(N*(N+1))/2;i+=2) {
+        for(j=0;j<M;j++) {
+            r[j] += ((int)_xij[i])*((int)F[Fi]) + ((int)_xij[i+1])*((int)F[Fi+1]);
+            Fi += 2;
+        }
+    }
+
+    for (i = 0; i < M; i++) {
+        fx[i] = ((r[i]%31)+31)%31;
     }
 }
