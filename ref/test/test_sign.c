7c99011ca5063a91238842bad5bc2d443870b04e
Joost Rijneveld <joost@joostrijneveld.nl>
2018-03-07 15:06:54 +0100

    Ensure overlapping m = sm is allowed

--- ref/test/test_sign.c
+++ ref/test/test_sign.c
@@ -13,7 +13,9 @@ int main()
     unsigned char m[MLEN];
     unsigned char m_out[SIG_LEN + MLEN];
     unsigned char sm[SIG_LEN + MLEN];
+    unsigned char sm2[SIG_LEN + MLEN];
     unsigned long long smlen;
+    unsigned long long smlen2;
     unsigned long long mlen;
     int i;
 
@@ -24,6 +26,7 @@ int main()
         crypto_sign_keypair(pk, sk);
         randombytes(m, MLEN);
         crypto_sign(sm, &smlen, m, MLEN, sk);
+        crypto_sign(sm2, &smlen2, m, MLEN, sk);
 
         if (smlen != SIG_LEN + MLEN) {
             printf("  FAIL! Signature length does not match (%llu / %d)!\n",
@@ -32,6 +35,14 @@ int main()
         }
         else if (crypto_sign_open(m_out, &mlen, sm, smlen, pk)) {
             printf("  FAIL! Signature did not verify correctly!\n");
+            return 1;
+        }
+        else if (memcmp(sm2, sm, smlen)) {
+            printf("  FAIL! Signature is not deterministic!\n");
+            return 1;
+        }
+        else if (crypto_sign_open(sm, &mlen, sm, smlen, pk)) {
+            printf("  FAIL! Signature couldn't be verified in-place!\n");
             return 1;
         }
         else if (mlen != MLEN) {
